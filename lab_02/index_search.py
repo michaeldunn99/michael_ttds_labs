0b9fc2b974d8bc2a3b0cc34b923184e5df8418ed
michaeldunn99 <michaelaidendunn@gmail.com>
2024-10-14 13:03:02 +0100

    Starting to add proximity search

--- lab_02/index_search.py
+++ lab_02/index_search.py
@@ -54,9 +54,9 @@ def or_operator(word_1, word_2, word_present):
         print("False")
         return False, set()
 
-def phrase_search(word_present, apply_stemming):
-    word_list = input("Which phrase would you like to search?: ")
-    word_list = preprocessing.remove_alphanumeric(preprocessing.lower_case(word_list)).split()
+def phrase_search(inverted_index, text_input_string, apply_stemming):
+    word_present = word_present_function(inverted_index)
+    word_list = preprocessing.remove_alphanumeric(preprocessing.lower_case(text_input_string)).split()
     if apply_stemming:
         word_list = preprocessing.my_porter_stemmer(word_list)
     #First loop through the word list to check each of our words are actually there
@@ -66,14 +66,13 @@ def phrase_search(word_present, apply_stemming):
         word_present_boolean, word_dict = word_present(word)
         if not word_present_boolean:
             print("No such phrase!")
-            return False
+            return False, {}
         else:
             word_dicts.append(word_dict)
     document_set = word_dicts[0]["document_set"]
 
     #We are going to maintain our first word position dict for finding phrases
     #potential_matching_position_dict = word_dicts[0]["position_dict"]
-    current_word_position = 0
     potential_positions_dict = {document:word_dicts[0]["position_dict"][document].copy() for document in document_set}
     for i in range(len(word_list)-1):
         docs_with_no_matches = 0
@@ -101,7 +100,7 @@ def phrase_search(word_present, apply_stemming):
         
             if docs_with_no_matches == len(potential_positions_dict.keys()):
                 print("No such phrase!")
-                return False
+                return False, {}
         
         for doc in docs_to_remove:
             document_set.remove(doc)
@@ -109,13 +108,40 @@ def phrase_search(word_present, apply_stemming):
 
     if not potential_positions_dict:
         print("No such phrase!")
-        return False
-
-    sorted_docs_first_position = sorted([int(key) for key in potential_positions_dict.keys()])
+        return False, {}
+    
+    sorted_docs_first_position_keys = sorted([int(key) for key in potential_positions_dict.keys()])
         
-    for match_doc in sorted_docs_first_position:
-        print(f"Match in document {match_doc} at postion(s): {sorted(list(potential_positions_dict[str(match_doc)]))}")
-    return True, sorted_docs_first_position
+    
+    sorted_docs_first_positions_dict = {match_doc: sorted(list(potential_positions_dict[str(match_doc)])) for match_doc in sorted_docs_first_position_keys}
+        #print(f"Match in document {match_doc} at postion(s): {sorted_first_positions[match_doc]}")
+    print(sorted_docs_first_positions_dict)
+    return True, sorted_docs_first_positions_dict
+
+def proximity_search(inverted_index, tuple_of_two_phrases, apply_stemming):
+    list_of_phrase_present_tuples = []
+    for phrase in tuple_of_two_phrases:
+        phrase_present_tuple = phrase_search(inverted_index, phrase, apply_stemming)
+        if phrase_present_tuple[0] == False:
+            return False, {}
+        else:
+            list_of_phrase_present_tuples.append(phrase_present_tuple)
+    
+    #Now we have a list of length two, each consisting of a tuple (True, phrase_position_dict)
+    #where phrase_position_dict is a dictionary with document numbers as keys and positions of that phrase
+    #in the document as values
+
+    #Now we need to check, for the documents (keys) that overlap, how close the phrases are (i.e. minimal distance between positions)
+
+    #Docs that both phrases are in are the overlapping set of keys
+    common_docs = set(list_of_phrase_present_tuples[0][1].keys()) & set(list_of_phrase_present_tuples[1][1].keys())
+
+    for doc in common_docs:
+        #work out the minimum distance between positions (i.e. minimum distance between two elements in different lists
+        #where both lists are sorted - think best to merge the lists then pick the minimum consecutive distance
+                                                                         
+    
+
 
 def main():
     remove_stop_words = False
